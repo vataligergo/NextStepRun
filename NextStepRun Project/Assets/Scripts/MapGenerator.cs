813ec25c8d9ca96f2f902c37acca8b30be8ce96f
vataligergo <vataligergo@gmail.com>
2018-01-17 17:47:44 +0100

    MapGenerator added
    has memory leak

---
+++ NextStepRun Project/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public GameObject[] MapParts;
+
+    private Transform playerTransform;
+    private float CreateParts = 0.0f;
+    private float PartLength = 15.0f;
+    private int NumberOfPartsOnScreen = 5;
+
+
+
+    // Use this for initialization
+    private void Start ()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    
+        for(int i = 0; i > NumberOfPartsOnScreen; i++)
+        {
+            MapGen();
+        }
+    }
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        if (playerTransform.position.z > (CreateParts - NumberOfPartsOnScreen * PartLength))
+        {
+            MapGen();
+        }
+    }
+
+    private void MapGen() // a MapParts tömbből egymás mögé pakolja a pályadarabokat (int i = -1)
+    {
+        GameObject go;
+        go = Instantiate(MapParts[0]) as GameObject;
+        go.transform.SetParent(transform);
+        go.transform.position = Vector3.forward * CreateParts;
+        CreateParts += PartLength;
+    }
+}
